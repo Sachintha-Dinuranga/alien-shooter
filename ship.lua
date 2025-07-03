4b834e9d14e2ad07fcb1f2f1d96850cea0f043af
Sachintha Dinuranga <117833055+Sachintha-Dinuranga@users.noreply.github.com>
2025-07-03 11:42:15 +0530

    add the ship and fix movement issue

--- ship.lua
+++ ship.lua
@@ -2,11 +2,12 @@ Ship = Object:extend()
 
 function Ship:new()
     -- load the image
-    self.image = love.graphics.newImage("sprites/spaceShip.png")
+    self.image = love.graphics.newImage("sprites/newShip.png")
     self.x = 350
     self.y = 490
     self.speed = 400
     self.width = self.image:getWidth()
+    print(self.width)
 
 end
 
@@ -23,13 +24,13 @@ function Ship:update(dt)
     if self.x < 0 then
         self.x = 0
     elseif self.x + self.width > window_width then
-        self.x = window_width - self.width
+        self.x = window_width - self.width -- issue here
     end
 end
 
 
 function Ship:draw()
     -- draw the image to the screen
-    -- love.graphics.draw(self.image, self.x, self.y, nil, 0.21, 0.21)
     love.graphics.draw(self.image, self.x, self.y)
+    -- love.graphics.draw(self.image, self.x, self.y)
 end
