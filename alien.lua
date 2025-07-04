e8a903f064a26dff7713ad91213b099ecc54280c
Sachintha Dinuranga <117833055+Sachintha-Dinuranga@users.noreply.github.com>
2025-07-04 12:15:32 +0530

    add alien movement and bounce back feature

--- alien.lua
+++ alien.lua
@@ -5,13 +5,24 @@ function Alien:new()
     self.x = 300
     self.y = 25
     self.speed = 100
+    self.width = self.image:getWidth()
 end
 
 function Alien:update(dt)
     self.x = self.x + self.speed * dt
+
+    local window_width = love.graphics.getWidth()
+
+    if self.x < 0 then
+        self.x = 0
+        self.speed = -(self.speed)
+    elseif self.x + self.width > window_width then
+        self.x = window_width - self.width
+        self.speed = -(self.speed)
+    end
 end
 
 
 function Alien:draw()
-    love.graphics.draw(self.image)
+    love.graphics.draw(self.image, self.x, self.y)
 end
--- main.lua
+++ main.lua
@@ -3,8 +3,13 @@ function love.load()
     -- load the ship
     Object = require "classic"
     require "ship"
+    require "alien"
 
     ship = Ship()
+    alien = Alien()
+
+    -- background image
+    background = love.graphics.newImage("sprites/space_copy.png")
 
 end
 
@@ -12,10 +17,18 @@ end
 -- update
 function love.update(dt)
     ship:update(dt)
+    alien:update(dt)
 end
 
 
 -- draw
 function love.draw()
+    -- background image
+    love.graphics.draw(background, 0, 0)
+
+    -- draw the ship
     ship:draw()
+
+    -- draw the alien
+    alien:draw()
 end
